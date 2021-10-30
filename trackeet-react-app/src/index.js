663cec9eb6fa5c0ed35836b6430c878e02aa9ecd
Guy Porges <guyporg@gmail.com>
2021-10-30 20:11:11 +0300

    revert

--- trackeet-react-app/src/index.js
+++ trackeet-react-app/src/index.js
@@ -1,12 +1,24 @@
+<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "../src/containers/app/App";
+=======
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+>>>>>>> parent of 56e2ee6... initial structure build
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+<<<<<<< HEAD
 
   document.getElementById("root")
+=======
+  document.getElementById('root')
+>>>>>>> parent of 56e2ee6... initial structure build
 );
