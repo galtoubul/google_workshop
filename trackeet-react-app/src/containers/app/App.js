d2143ea57385de8458760d9a597a26c994a12990
Shay Abramovich <shay@mail.tau.ac.il>
2021-11-06 19:56:58 +0200

    finish

--- trackeet-react-app/src/containers/app/App.js
+++ trackeet-react-app/src/containers/app/App.js
@@ -1,32 +1,10 @@
-import { useState } from "react";
+//import { Login } from "../../components/header/buttons/Login";
+//import { Information } from "../../components/header/buttons/Information";
+//import { Notifications } from "../../components/header/buttons/Notifications";
+//import { Box } from "@mui/material";
+import { Header } from "../../components/header/Header";
 import "./App.css";
-import NonDetailedForm from "../forms/non_detailed/NonDetailedForm";
-
-const ViewOne = ({ onClick }) => (
-  <div>
-    Buckets <br />
-    <button onClick={() => onClick("view2")}>Go to Non Detailed Form</button>
-  </div>
-);
-
-const ViewTwo = ({ onClick }) => (
-  <div>
-    Non Detailed Form <br />
-    <button onClick={() => onClick("view1")}>Go to Buckets</button>
-    <NonDetailedForm />
-  </div>
-);
 
 export const App = () => {
-  const [currentView, setCurrentView] = useState("view1");
-
-  return (
-    <div>
-      {currentView === "view1" ? (
-        <ViewOne onClick={(page) => setCurrentView(page)} />
-      ) : (
-        <ViewTwo onClick={(page) => setCurrentView(page)} />
-      )}
-    </div>
-  );
+  return <Header />;
 };
