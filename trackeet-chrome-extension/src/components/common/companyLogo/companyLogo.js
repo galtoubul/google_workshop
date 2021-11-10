2af5b77499cab8c279d826a7c01909e32891a0b1
Shay Abramovich <shay@mail.tau.ac.il>
2021-11-10 16:39:12 +0200

    Chrome Extension

--- trackeet-chrome-extension/src/components/common/companyLogo/companyLogo.js
+++
@@ -1,5 +0,0 @@
-import { getLogo } from "./getLogo";
-
-export const CompanyLogo = (props) => {
-  return getLogo(props.shopName);
-};
--- trackeet-chrome-extension/src/components/common/kanban/kanbanStyle.js
+++
@@ -1,8 +0,0 @@
-import { flexbox } from "@chakra-ui/react";
-
-export const boardStyle = {
-  display: flexbox,
-  alignItems: "stretch",
-  justifyContent: "center",
-  backgroundColor: "white",
-};
