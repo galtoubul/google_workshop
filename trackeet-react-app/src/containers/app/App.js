0a79bbed2de642bcc6077e2173921ab41d70e4d2
galtoubul <galtoubul@gmail.com>
2021-11-20 16:35:56 +0200

    trying to solve bug in google auth

--- trackeet-react-app/src/containers/app/App.js
+++ trackeet-react-app/src/containers/app/App.js
@@ -3,18 +3,17 @@ import LocalizationProvider from "@mui/lab/LocalizationProvider";
 import { Page } from "../page/Page";
 import { Header } from "../../components/header/Header";
 import ThemColors from "../theme/ThemColors";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { LoginPage } from "../logInPage/LoginPage";
 import { useGoogleLogout } from "react-google-login";
 
 export const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [clientId, setClientId] = useState("");
 
   const login = () => {
     setIsLoggedIn(true);
   };
-
-  const clientId = process.env.REACT_APP_TRACKEET_GOOGLE_AUTH_CLIENT_ID;
 
   const { signOut } = useGoogleLogout({
     clientId,
@@ -25,11 +24,23 @@ export const App = () => {
     signOut();
   };
 
+  useEffect(() => {
+    fetch("/api/client_id")
+      .then((res) => res.json())
+      .then((data) => {
+        setClientId(data.clientId);
+      });
+  }, []);
+
   return (
     <ThemColors>
       <LocalizationProvider dateAdapter={AdapterDateFns}>
         <Header isLoggedIn={isLoggedIn} logOut={logOut} />
-        {isLoggedIn ? <Page></Page> : <LoginPage login={login} />}
+        {isLoggedIn ? (
+          <Page></Page>
+        ) : (
+          <LoginPage clientId={clientId} login={login} />
+        )}
       </LocalizationProvider>
     </ThemColors>
   );
