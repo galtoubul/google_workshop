c35a43a928516b45520ee031783d4d6bd551dde6
galtoubul <galtoubul@gmail.com>
2021-11-20 12:42:40 +0200

    login with google is working

--- backend/web-server/api.py
+++ backend/web-server/api.py
@@ -1,8 +1,8 @@
 from flask import Flask
 import time
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../../trackeet-react-app/build", static_url_path="/")
 
-@app.route('/time')
+@app.route('/api/time')
 def hello_world():
     return {'time': time.time()}
