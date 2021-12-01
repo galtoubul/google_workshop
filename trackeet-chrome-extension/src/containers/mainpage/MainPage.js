896e35e32f6b821acf6a3aaede524b573e6400bb
Shay Abramovich <shay@mail.tau.ac.il>
2021-12-01 16:59:48 +0200

    The extension successfully scans Amazon. Now I just need to change the structure of the code to generic functions

--- trackeet-chrome-extension/src/containers/mainpage/MainPage.js
+++ trackeet-chrome-extension/src/containers/mainpage/MainPage.js
@@ -5,6 +5,7 @@ import Button from "@mui/material/Button";
 import { BiLinkExternal } from "react-icons/bi";
 import NonDetailedForm from "../forms/non_detailed/NonDetailedForm";
 import { useState } from "react";
+// import Extractor_Amazon from "../../scripts/Extractor_Amazon";
 
 export const ADD_NEW_ORDER = "Add new order";
 
@@ -29,9 +30,37 @@ export const MainPage = () => {
         variant="outlined"
         sx={{ margin: "2% 5%", display: "flex", justifyContent: "center" }}
         endIcon={<BiLinkExternal />}
+        onClick={() => {
+          // eslint-disable-next-line no-undef
+          chrome.tabs.executeScript(null, {
+            file: "Extractor_Amazon.js",
+          });
+        }}
+      >
+        Scan Habibi
+      </Button>
+      <Button
+        variant="outlined"
+        sx={{ margin: "2% 5%", display: "flex", justifyContent: "center" }}
+        endIcon={<BiLinkExternal />}
       >
         Trackeet
       </Button>
     </div>
   );
 };
+
+// chrome.tabs.query(null, function (tabs) {
+//     // eslint-disable-next-line prefer-destructuring
+//     const tab = tabs[0];
+//     // eslint-disable-next-line no-undef
+//     chrome.tabs.executeScript(
+//         tab.id,
+//         {
+//             code: 'alert("hi!")',
+//         },
+//         (win) => {
+//             console.log(win);
+//         }
+//     );
+// });
