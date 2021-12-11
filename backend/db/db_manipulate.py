2c0810c9a16a7e71be521e2a19567928e5ba4b36
galtoubul <galtoubul@gmail.com>
2021-12-11 18:04:37 +0200

    updateCard is working

--- backend/db/db_manipulate.py
+++ backend/db/db_manipulate.py
@@ -115,3 +115,100 @@ def add_card (data):
     except SQLC.IntegrityError as err:
         return ('ERROR: failed to insert values: %s', err)
     return 'Succesful insertion! {} rows were affacted'.format(cursor.rowcount)
+
+
+db_to_fe_dict = {
+    'OrderNumber': 'order_serial_code',
+    'OrderName': 'order_name',
+    'CompanyName': 'company',
+    'CustomerId': 'user_id',
+    'Url': 'order_url',
+    'Bucket': 'timeline_position',
+    'Price': 'price',
+    'Currency': 'currency',
+    'OrderDate': 'order_date',
+    'EstimatedArrivingDate': 'estimated_arrival_date',
+    'Notes': 'notes'
+}
+
+
+def get_fe_param_to_query_param(data):
+    order_serial_code = None if 'order_serial_code' not in data else int(data.get('order_serial_code'))
+    order_name = None if 'order_name' not in data else data.get('order_name')
+    company = None if 'company' not in data else data.get('company')
+    user_id = None if 'user_id' not in data else int(data.get('user_id'))
+    order_url = None if 'order_url' not in data else data.get('order_url')
+    timeline_position = 'Wishlist' if 'timeline_position' not in data else data.get('price')
+    price = float(data.get('price')) if 'price' in data else None
+    currency = None if 'currency' not in data else data.get('currency')
+    order_date = None if 'order_date' not in data else data.get('order_date')
+    estimated_arrival_date = None if 'estimated_arrival_date' not in data else data.get('estimated_arrival_date')
+    notes = None if 'notes' not in data else data.get('notes')
+
+    fe_param_to_query_param = {
+        'order_serial_code': order_serial_code,
+        'order_name': order_name,
+        'company': company,
+        'user_id': user_id,
+        'order_url': order_url,
+        'timeline_position': timeline_position,
+        'price': price,
+        'currency': currency,
+        'order_date': order_date,
+        'estimated_arrival_date': estimated_arrival_date,
+        'notes': notes
+    }
+
+    return fe_param_to_query_param
+
+
+def update_card(customer_id, compamy_name, order_number, data):
+    fe_param_to_query_param = get_fe_param_to_query_param(data)
+
+    try:       
+        update_query = 'UPDATE Trackeet.Orders SET '
+
+        query_params_dict = {}
+        is_first = True
+        for (db_format, fe_format) in db_to_fe_dict.items():
+            if fe_format in data:
+                if not is_first:
+                    update_query += ', '
+                update_query += f'{db_format} = %({fe_format})s'
+                query_params_dict[fe_format] = fe_param_to_query_param[fe_format]
+                is_first = False
+
+        query_params_dict['customer_id'] = int(customer_id)
+        query_params_dict['compamy_name'] = compamy_name
+        query_params_dict['order_number'] = int(order_number)
+
+        print(query_params_dict)
+
+        update_query += (""" WHERE CustomerId = %(customer_id)s   AND
+                                   CompanyName = %(compamy_name)s AND
+                                   OrderNumber = %(order_number)s""")
+
+        print(update_query)
+
+        cursor.execute(update_query, query_params_dict)
+        db_con.commit()
+
+    except SQLC.IntegrityError as err:
+        return ('ERROR: failed to insert values: %s', err)
+    return 'Succesful insertion! {} rows were affacted'.format(cursor.rowcount)
+
+
+
+# """
+#                           OrderNumber = %(order_number)s,
+#                           OrderName = %(order_name)s,
+#                           CompanyName = %(company_name)s,
+#                           CustomerId = %(customer_id)s,
+#                           Url = %(url)s,
+#                           Bucket = %(bucket)s,
+#                           Price = %(price)s,
+#                           Currency = %(currency)s,
+#                           OrderDate = %(order_date)s,
+#                           EstimatedArrivingDate = %(estimated_arriving_date)s,
+#                           Notes = %(notes)s)""",
+                                
