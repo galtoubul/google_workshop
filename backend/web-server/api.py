76aa7039063d47cf616533f5d6ee14d0ebfda838
galtoubul <galtoubul@gmail.com>
2021-12-29 08:13:04 +0200

    fixing a bug in the use of tracking more + adding support for the chrome ext's client id

--- backend/web-server/api.py
+++ backend/web-server/api.py
@@ -9,7 +9,7 @@ from google.auth.transport import requests
 sys.path.append(os.path.abspath('../db'))
 import db_manipulate as db
 sys.path.append(os.path.abspath('../'))
-from config import client_id, ERR
+from config import web_app_client_id, chrome_ext_client_id, ERR
 from auto_track import getDeliveryStatus
 
 
@@ -18,10 +18,14 @@ CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 
-def get_user_info(token_id):
-    print(f'\n\nget_user_info\ntoken_id = {token_id}', flush=True)
+def get_user_info(token_id, use_chrome_ext_client_id=False):
+    print(f'\n\nget_user_info\ntoken_id = {token_id}\nuse_chrome_ext_client_id = {use_chrome_ext_client_id}', flush=True)
+
     try:
-        return id_token.verify_oauth2_token(token_id, requests.Request(), client_id)
+        if use_chrome_ext_client_id:
+            return id_token.verify_oauth2_token(token_id, requests.Request(), chrome_ext_client_id)
+        return id_token.verify_oauth2_token(token_id, requests.Request(), web_app_client_id)
+
     except ValueError as err:
         print(f'\ninvalid token: {err}', flush=True)
         return ERR
@@ -59,6 +63,7 @@ def update_cards_if_needed(not_updated_cards):
     for card in not_updated_cards:
         serial_code = card['order_serial_code']
         bucket = card['timeline_position']
+        
         curr_bucket = getDeliveryStatus(serial_code)
         if curr_bucket == ERR:
             continue
@@ -66,8 +71,8 @@ def update_cards_if_needed(not_updated_cards):
         if curr_bucket in track_api_to_bucket and track_api_to_bucket[curr_bucket] != bucket:
             data = {
                 'card_id': card['card_id'], 
-                'order_name':card['order_name'] , 
-                'timeline_position': curr_bucket
+                'order_name': card['order_name'], 
+                'timeline_position': track_api_to_bucket[curr_bucket]
             }
             db.update_card(data)
 
@@ -78,46 +83,47 @@ def retrieve_cards():
     print(f'\n\ngetCards\nheaders = {request.headers}\nrequest.data = {request.data}', flush=True)
 
     data_dict = json_bytes_to_dict(request.data)
-    print('data_dict = ', data_dict, flush=True)
+    print(f'\n\ndata_dict = {data_dict}', flush=True)
 
     if 'token_id' not in data_dict:
         return get_response({'response':f'Didn\'t supplied token_id'}, 400)
 
-    user_info = get_user_info(data_dict['token_id'])
+    use_chrome_ext_client_id = True if 'use_chrome_ext_client_id' in data_dict else False
+    user_info = get_user_info(data_dict['token_id'], use_chrome_ext_client_id)
+
     if user_info == ERR:
         return get_response({'response':'Couldn\'t authenticate user via Google'}, 403)
-    user_id = user_info['sub']
-    print(f'user_id = {user_id}', flush=True)
 
-    not_updated_cards_serial_codes = db.get_not_updated_cards_serial_codes(user_id)
-    update_cards_if_needed(not_updated_cards_serial_codes)
+    user_id = user_info['sub']
+    print(f'\n\nuser_id = {user_id}', flush=True)
+
+    not_updated_cards = db.get_not_updated_cards(user_id)
+    update_cards_if_needed(not_updated_cards)
 
     bucket = None if 'timeline_position' not in request.args else request.args.get('timeline_position')
-    print(f'bucket = {bucket}', flush=True)
-
     cards = db.get_cards(user_id, bucket)
     
-    print(f'cards = {cards}', flush=True)
+    print(f'\n\ncards = {cards}', flush=True)
     return get_response(cards)
 
 
 @app.route('/api/addCard', methods=['POST'])
 @cross_origin()
 def add_card():
-    print(f'\n\naddCard\nrequest.data = {request.data}', flush=True)
+    print(f'\n\naddCard\nheaders = {request.headers}\nrequest.data = {request.data}', flush=True)
 
     data_dict = json_bytes_to_dict(request.data)
-    print(f'data_dict = {data_dict}', flush=True)
+    print(f'\n\ndata_dict = {data_dict}', flush=True)
 
     for necessary_val in ['token_id', 'order_name', 'card_id']:
         if necessary_val not in data_dict:
             return get_response({'response':f'Didn\'t supplied {necessary_val}'}, 400)
 
-    user_info = get_user_info(data_dict['token_id'])
+    use_chrome_ext_client_id = True if 'use_chrome_ext_client_id' in data_dict else False
+    user_info = get_user_info(data_dict['token_id'], use_chrome_ext_client_id)
+
     if user_info == ERR:
         return get_response({'response':'Couldn\'t authenticate user via Google'}, 403)
-
-    print(f'user_info = {user_info}', flush=True)
 
     res = db.add_card(data_dict, user_info)
     return get_response(res)
@@ -126,21 +132,22 @@ def add_card():
 @app.route('/api/updateCard', methods=['POST'])
 @cross_origin()
 def update_card():
-    print(f'\n\nupdate_card\nrequest.data = {request.data}', flush=True)
+    print(f'\n\nupdate_card\nheaders = {request.headers}\nrequest.data = {request.data}', flush=True)
 
     data_dict = json_bytes_to_dict(request.data)
-    print(f'data_dict = {data_dict}', flush=True)
+    print(f'\n\ndata_dict = {data_dict}', flush=True)
 
     for necessary_val in ['token_id', 'card_id', 'order_name']:
         if necessary_val not in data_dict:
             return get_response({'response':f'Didn\'t supplied {necessary_val}'}, 400)
 
-    user_info = get_user_info(data_dict['token_id'])
+    use_chrome_ext_client_id = True if 'use_chrome_ext_client_id' in data_dict else False
+    user_info = get_user_info(data_dict['token_id'], use_chrome_ext_client_id)
+
     if user_info == ERR:
         return get_response({'response':'Couldn\'t authenticate user via Google'}, 403)
-    user_id = user_info['sub']
-    data_dict['user_id'] = user_id
 
+    data_dict['user_id'] = user_info['sub']
     res = db.update_card(data_dict)
 
     resp = get_response(res)
@@ -152,16 +159,18 @@ def update_card():
 
 @app.route('/api/deleteCard', methods=['POST'])
 def delete_card():
-    print('\n\ndeleteCard\ndata = {request.data}', flush=True)
+    print('\n\ndeleteCard\nheaders = {request.headers}\ndata = {request.data}', flush=True)
 
     data_dict = json_bytes_to_dict(request.data)
-    print('data_dict = ', data_dict, flush=True)
+    print('\n\ndata_dict = ', data_dict, flush=True)
 
     for necessary_val in ['token_id', 'card_id', 'order_name']:
         if necessary_val not in data_dict:
             return get_response({'response':f'Didn\'t supplied {necessary_val}'}, 400)
 
-    user_info = get_user_info(data_dict['token_id'])
+    use_chrome_ext_client_id = True if 'use_chrome_ext_client_id' in data_dict else False
+    user_info = get_user_info(data_dict['token_id'], use_chrome_ext_client_id)
+
     if user_info == ERR:
         return get_response({'response':'Couldn\'t authenticate user via Google'}, 403)
 
