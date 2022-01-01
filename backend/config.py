e4ba0928e065ecefb6d87328e1c0ce459b5a40a4
galtoubul <galtoubul@gmail.com>
2022-01-01 13:20:25 +0200

    adding flaskext.mysql

--- backend/config.py
+++ backend/config.py
@@ -6,6 +6,7 @@ load_dotenv()
 db_host = os.getenv('DB_HOST')
 db_user = os.getenv('DB_USER')
 db_password = os.getenv('DB_PASSWORD')
+db_database = os.getenv('DB_DATABASE')
 
 web_app_client_id = os.getenv('REACT_APP_TRACKEET_GOOGLE_AUTH_CLIENT_ID')
 chrome_ext_client_id = os.getenv('TRACKEET_CHROME_EXTENSION_GOOGLE_AUTH_CLIENT_ID')
